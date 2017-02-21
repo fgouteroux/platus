41f7cbe6518065c07169cbf9fbe3bfc08d5d72fe
François Gouteroux <francois.gouteroux@gmail.com>
2017-02-21 11:56:19 +0100

    add infos on user connected and config settings in nav bar

--- platus/web.py
+++ platus/web.py
@@ -8,6 +8,9 @@ from flask import Blueprint, render_template, request, jsonify
 from platus import controllers
 from platus.auth import auth, get_roles
 
+# Import third party libs
+from flask import current_app as app
+
 web = Blueprint("web", __name__)
 logger = logging.getLogger(__name__)
 
@@ -17,8 +20,14 @@ logger = logging.getLogger(__name__)
 def index():
     try:
         if request.method == "GET":
-            roles = get_roles(auth.username())
-            return render_template('index.html', plugins=controllers.services_status(roles))
+            current_user = auth.username()
+            roles = get_roles(current_user)
+
+            return render_template(
+                'index.html',
+                config=app.config,
+                current_user=current_user,
+                services=controllers.services_status(roles))
     except:
         raise
         return jsonify({'result': 'none'}), 401
